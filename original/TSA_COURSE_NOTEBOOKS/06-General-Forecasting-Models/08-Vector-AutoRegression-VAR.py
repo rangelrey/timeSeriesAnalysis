ebb40cf60c5c17fdcf3b45066b89bdd035048b9a
rangelrey <rogerangelrrp@gmail.com>
2020-05-06 08:20:13 +0200

    var

---
+++ original/TSA_COURSE_NOTEBOOKS/06-General-Forecasting-Models/08-Vector-AutoRegression-VAR.py
@@ -0,0 +1,500 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ___
+# 
+# <a href='http://www.pieriandata.com'><img src='../Pierian_Data_Logo.png'/></a>
+# ___
+# <center><em>Copyright Pierian Data</em></center>
+# <center><em>For more information, visit us at <a href='http://www.pieriandata.com'>www.pieriandata.com</a></em></center>
+
+# # VAR(p)
+# ## Vector Autoregression
+# In our previous SARIMAX example, the forecast variable $y_t$ was influenced by the exogenous predictor variable, but not vice versa. That is, the occurrence of a holiday affected restaurant patronage but not the other way around.
+# 
+# However, there are some cases where variables affect each other. <a href='https://otexts.com/fpp2/VAR.html'>Forecasting: Principles and Practice</a> describes a case where changes in personal consumption expenditures $C_t$ were forecast based on changes in personal disposable income $I_t$.
+# > However, in this case a bi-directional relationship may be more suitable: an increase in $I_t$ will lead to an increase in $C_t$ and vice versa.<br>An example of such a situation occurred in Australia during the Global Financial Crisis of 2008–2009. The Australian government issued stimulus packages that included cash payments in December 2008, just in time for Christmas spending. As a result, retailers reported strong sales and the economy was stimulated. Consequently, incomes increased.
+# 
+# Aside from investigating multivariate time series, vector autoregression is used for
+# * <a href='https://www.statsmodels.org/devel/vector_ar.html#impulse-response-analysis'>Impulse Response Analysis</a> which involves the response of one variable to a sudden but temporary change in another variable
+# * <a href='https://www.statsmodels.org/devel/vector_ar.html#forecast-error-variance-decomposition-fevd'>Forecast Error Variance Decomposition (FEVD)</a> where the proportion of the forecast variance of one variable is attributed to the effect of other variables
+# * <a href='https://www.statsmodels.org/devel/vector_ar.html#dynamic-vector-autoregressions'>Dynamic Vector Autoregressions</a> used for estimating a moving-window regression for the purposes of making forecasts throughout the data sample
+# 
+# ### Formulation
+# We've seen that an autoregression AR(p) model is described by the following:
+# 
+# &nbsp;&nbsp;&nbsp;&nbsp; $y_{t} = c + \phi_{1}y_{t-1} + \phi_{2}y_{t-2} + \dots + \phi_{p}y_{t-p} + \varepsilon_{t}$
+# 
+# where $c$ is a constant, $\phi_{1}$ and $\phi_{2}$ are lag coefficients up to order $p$, and $\varepsilon_{t}$ is white noise.
+
+# A $K$-dimensional VAR model of order $p$, denoted <strong>VAR(p)</strong>, considers each variable $y_K$ in the system.<br>
+# 
+# For example, The system of equations for a 2-dimensional VAR(1) model is:
+# 
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{1,t} = c_1 + \phi_{11,1}y_{1,t-1} + \phi_{12,1}y_{2,t-1} + \varepsilon_{1,t}$<br>
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{2,t} = c_2 + \phi_{21,1}y_{1,t-1} + \phi_{22,1}y_{2,t-1} + \varepsilon_{2,t}$
+# 
+# where the coefficient $\phi_{ii,l}$ captures the influence of the $l$th lag of variable $y_i$ on itself,<br>
+# the coefficient $\phi_{ij,l}$ captures the influence of the $l$th lag of variable $y_j$ on $y_i$,<br>
+# and $\varepsilon_{1,t}$ and $\varepsilon_{2,t}$ are white noise processes that may be correlated.<br>
+# 
+# Carrying this further, the system of equations for a 2-dimensional VAR(3) model is:
+# 
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{1,t} = c_1 + \phi_{11,1}y_{1,t-1} + \phi_{12,1}y_{2,t-1} + \phi_{11,2}y_{1,t-2} + \phi_{12,2}y_{2,t-2} + \phi_{11,3}y_{1,t-3} + \phi_{12,3}y_{2,t-3} + \varepsilon_{1,t}$<br>
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{2,t} = c_2 + \phi_{21,1}y_{1,t-1} + \phi_{22,1}y_{2,t-1} + \phi_{21,2}y_{1,t-2} + \phi_{22,2}y_{2,t-2} + \phi_{21,3}y_{1,t-3} + \phi_{22,3}y_{2,t-3} + \varepsilon_{2,t}$<br><br>
+# 
+# and the system of equations for a 3-dimensional VAR(2) model is:
+# 
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{1,t} = c_1 + \phi_{11,1}y_{1,t-1} + \phi_{12,1}y_{2,t-1} + \phi_{13,1}y_{3,t-1} + \phi_{11,2}y_{1,t-2} + \phi_{12,2}y_{2,t-2} + \phi_{13,2}y_{3,t-2} + \varepsilon_{1,t}$<br>
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{2,t} = c_2 + \phi_{21,1}y_{1,t-1} + \phi_{22,1}y_{2,t-1} + \phi_{23,1}y_{3,t-1} + \phi_{21,2}y_{1,t-2} + \phi_{22,2}y_{2,t-2} + \phi_{23,2}y_{3,t-2} + \varepsilon_{2,t}$<br>
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{3,t} = c_3 + \phi_{31,1}y_{1,t-1} + \phi_{32,1}y_{2,t-1} + \phi_{33,1}y_{3,t-1} + \phi_{31,2}y_{1,t-2} + \phi_{32,2}y_{2,t-2} + \phi_{33,2}y_{3,t-2} + \varepsilon_{3,t}$<br><br>
+# 
+# The general steps involved in building a VAR model are:
+# * Examine the data
+# * Visualize the data
+# * Test for stationarity
+# * If necessary, transform the data to make it stationary
+# * Select the appropriate order <em>p</em>
+# * Instantiate the model and fit it to a training set
+# * If necessary, invert the earlier transformation
+# * Evaluate model predictions against a known test set
+# * Forecast the future
+# 
+# Recall that to fit a SARIMAX model we passed one field of data as our <em>endog</em> variable, and another for <em>exog</em>. With VAR, both fields will be passed in as <em>endog</em>.
+
+# <div class="alert alert-info"><h3>Related Functions:</h3>
+# <tt><strong>
+# <a href='https://www.statsmodels.org/stable/generated/statsmodels.tsa.vector_ar.var_model.VAR.html'>vector_ar.var_model.VAR</a></strong><font color=black>(endog[, exog, …])</font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fit VAR(p) process and do lag order selection<br>
+# <strong><a href='https://www.statsmodels.org/stable/generated/statsmodels.tsa.vector_ar.var_model.VARResults.html'>vector_ar.var_model.VARResults</a></strong><font color=black>(endog, …[, …])</font>&nbsp;&nbsp;Estimate VAR(p) process with fixed number of lags<br>
+# <strong><a href='https://www.statsmodels.org/stable/generated/statsmodels.tsa.vector_ar.dynamic.DynamicVAR.html'>vector_ar.dynamic.DynamicVAR</a></strong><font color=black>(data[, …])</font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Estimates time-varying vector autoregression (VAR(p)) using equation-by-equation least squares</tt>
+#    
+# <h3>For Further Reading:</h3>
+# <strong>
+# <a href='https://www.statsmodels.org/stable/vector_ar.html'>Statsmodels Tutorial:</a></strong>&nbsp;&nbsp;<font color=black>Vector Autoregressions</font><br>
+# <strong>
+# <a href='https://otexts.com/fpp2/VAR.html'>Forecasting: Principles and Practice:</a></strong>&nbsp;&nbsp;<font color=black>Vector Autoregressions</font><br>
+# <strong>
+# <a href='https://en.wikipedia.org/wiki/Vector_autoregression'>Wikipedia:</a></strong>&nbsp;&nbsp;<font color=black>Vector Autoregression</font>
+# </div>
+
+# ### Perform standard imports and load dataset
+# For this analysis we'll also compare money to spending. We'll look at the M2 Money Stock which is a measure of U.S. personal assets, and U.S. personal spending. Both datasets are in billions of dollars, monthly, seasonally adjusted. They span the 21 years from January 1995 to December 2015 (252 records).<br>
+# Sources: https://fred.stlouisfed.org/series/M2SL https://fred.stlouisfed.org/series/PCE
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Load specific forecasting tools
+from statsmodels.tsa.api import VAR, DynamicVAR
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tools.eval_measures import rmse
+
+# Ignore harmless warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+# Load datasets
+df = pd.read_csv('../UnusedData/M2SLMoneyStock.csv',index_col=0, parse_dates=True)
+df.index.freq = 'MS'
+
+sp = pd.read_csv('../UnusedData/PCEPersonalSpending.csv',index_col=0, parse_dates=True)
+sp.index.freq = 'MS'
+
+
+# ### Inspect the data
+
+# In[2]:
+
+
+df = df.join(sp)
+df.head()
+
+
+# In[3]:
+
+
+df = df.dropna()
+df.shape
+
+
+# ### Plot the source data
+
+# In[4]:
+
+
+title = 'M2 Money Stock vs. Personal Consumption Expenditures'
+ylabel='Billions of dollars'
+xlabel=''
+
+ax = df['Spending'].plot(figsize=(12,5),title=title,legend=True)
+ax.autoscale(axis='x',tight=True)
+ax.set(xlabel=xlabel, ylabel=ylabel)
+df['Money'].plot(legend=True);
+
+
+# ## Test for stationarity, perform any necessary transformations
+
+# In[5]:
+
+
+def adf_test(series,title=''):
+    """
+    Pass in a time series and an optional title, returns an ADF report
+    """
+    print(f'Augmented Dickey-Fuller Test: {title}')
+    result = adfuller(series.dropna(),autolag='AIC') # .dropna() handles differenced data
+    
+    labels = ['ADF test statistic','p-value','# lags used','# observations']
+    out = pd.Series(result[0:4],index=labels)
+
+    for key,val in result[4].items():
+        out[f'critical value ({key})']=val
+        
+    print(out.to_string())          # .to_string() removes the line "dtype: float64"
+    
+    if result[1] <= 0.05:
+        print("Strong evidence against the null hypothesis")
+        print("Reject the null hypothesis")
+        print("Data has no unit root and is stationary")
+    else:
+        print("Weak evidence against the null hypothesis")
+        print("Fail to reject the null hypothesis")
+        print("Data has a unit root and is non-stationary")
+
+
+# In[6]:
+
+
+adf_test(df['Money'],title='Money')
+
+
+# In[7]:
+
+
+adf_test(df['Spending'], title='Spending')
+
+
+# Neither variable is stationary, so we'll take a first order difference of the entire DataFrame and re-run the augmented Dickey-Fuller tests. It's advisable to save transformed values in a new DataFrame, as we'll need the original when we later invert the transormations and evaluate the model.
+
+# In[8]:
+
+
+df_transformed = df.diff()
+
+
+# In[9]:
+
+
+df_transformed = df_transformed.dropna()
+adf_test(df_transformed['Money'], title='MoneyFirstDiff')
+print()
+adf_test(df_transformed['Spending'], title='SpendingFirstDiff')
+
+
+# Since Money is not yet stationary, we'll apply second order differencing to both series so they retain the same number of observations
+
+# In[10]:
+
+
+df_transformed = df_transformed.diff().dropna()
+adf_test(df_transformed['Money'], title='MoneySecondDiff')
+print()
+adf_test(df_transformed['Spending'], title='SpendingSecondDiff')
+
+
+# In[11]:
+
+
+df_transformed.head()
+
+
+# In[12]:
+
+
+len(df_transformed)
+
+
+# ### Train/test split
+# It will be useful to define a number of observations variable for our test set. For this analysis, let's use 12 months.
+
+# In[13]:
+
+
+nobs=12
+train, test = df_transformed[0:-nobs], df_transformed[-nobs:]
+
+
+# In[14]:
+
+
+print(train.shape)
+print(test.shape)
+
+
+# ## VAR Model Order Selection
+# We'll fit a series of models using the first seven p-values, and base our final selection on the model that provides the lowest AIC and BIC scores.
+
+# In[15]:
+
+
+for i in [1,2,3,4,5,6,7]:
+    model = VAR(train)
+    results = model.fit(i)
+    print('Order =', i)
+    print('AIC: ', results.aic)
+    print('BIC: ', results.bic)
+    print()
+
+
+# In[16]:
+
+
+model = VAR(train)
+for i in [1,2,3,4,5,6,7]:
+    results = model.fit(i)
+    print('Order =', i)
+    print('AIC: ', results.aic)
+    print('BIC: ', results.bic)
+    print()
+
+
+# The VAR(5) model seems to return the lowest combined scores.<br>
+# Just to verify that both variables are included in the model we can run <tt>.endog_names</tt>
+
+# In[17]:
+
+
+model.endog_names
+
+
+# ## Fit the VAR(5) Model
+
+# In[18]:
+
+
+results = model.fit(5)
+results.summary()
+
+
+# ## Predict the next 12 values
+# Unlike the VARMAX model we'll use in upcoming sections, the VAR <tt>.forecast()</tt> function requires that we pass in a lag order number of previous observations as well. Unfortunately this forecast tool doesn't provide a DateTime index - we'll have to do that manually.
+
+# In[19]:
+
+
+lag_order = results.k_ar
+lag_order
+
+
+# In[20]:
+
+
+z = results.forecast(y=train.values[-lag_order:], steps=12)
+z
+
+
+# In[21]:
+
+
+test
+
+
+# In[22]:
+
+
+idx = pd.date_range('1/1/2015', periods=12, freq='MS')
+df_forecast = pd.DataFrame(z, index=idx, columns=['Money2d','Spending2d'])
+df_forecast
+
+
+# ## Invert the Transformation
+# Remember that the forecasted values represent second-order differences. To compare them to the original data we have to roll back each difference. To roll back a first-order difference we take the most recent value on the training side of the original series, and add it to a cumulative sum of forecasted values. When working with second-order differences we first must perform this operation on the most recent first-order difference.
+# 
+# Here we'll use the <tt>nobs</tt> variable we defined during the train/test/split step.
+
+# In[23]:
+
+
+# Add the most recent first difference from the training side of the original dataset to the forecast cumulative sum
+df_forecast['Money1d'] = (df['Money'].iloc[-nobs-1]-df['Money'].iloc[-nobs-2]) + df_forecast['Money2d'].cumsum()
+
+# Now build the forecast values from the first difference set
+df_forecast['MoneyForecast'] = df['Money'].iloc[-nobs-1] + df_forecast['Money1d'].cumsum()
+
+
+# In[24]:
+
+
+# Add the most recent first difference from the training side of the original dataset to the forecast cumulative sum
+df_forecast['Spending1d'] = (df['Spending'].iloc[-nobs-1]-df['Spending'].iloc[-nobs-2]) + df_forecast['Spending2d'].cumsum()
+
+# Now build the forecast values from the first difference set
+df_forecast['SpendingForecast'] = df['Spending'].iloc[-nobs-1] + df_forecast['Spending1d'].cumsum()
+
+
+# In[25]:
+
+
+df_forecast
+
+
+# ## Plot the results
+# The VARResults object offers a couple of quick plotting tools:
+
+# In[26]:
+
+
+results.plot();
+
+
+# In[27]:
+
+
+results.plot_forecast(12);
+
+
+# But for our investigation we want to plot predicted values against our test set.
+
+# In[28]:
+
+
+df['Money'][-nobs:].plot(figsize=(12,5),legend=True).autoscale(axis='x',tight=True)
+df_forecast['MoneyForecast'].plot(legend=True);
+
+
+# In[29]:
+
+
+df['Spending'][-nobs:].plot(figsize=(12,5),legend=True).autoscale(axis='x',tight=True)
+df_forecast['SpendingForecast'].plot(legend=True);
+
+
+# ### Evaluate the model
+# 
+# &nbsp;&nbsp;&nbsp;&nbsp;$RMSE = \sqrt{{\frac 1 L} \sum\limits_{l=1}^L (y_{T+l} - \hat y_{T+l})^2}$<br><br>
+# where $T$ is the last observation period and $l$ is the lag.
+
+# In[30]:
+
+
+RMSE1 = rmse(df['Money'][-nobs:], df_forecast['MoneyForecast'])
+print(f'Money VAR(5) RMSE: {RMSE1:.3f}')
+
+
+# In[31]:
+
+
+RMSE2 = rmse(df['Spending'][-nobs:], df_forecast['SpendingForecast'])
+print(f'Spending VAR(5) RMSE: {RMSE2:.3f}')
+
+
+# ## Let's compare these results to individual AR(5) models
+
+# In[33]:
+
+
+from statsmodels.tsa.ar_model import AR,ARResults
+
+
+# ### Money
+
+# In[34]:
+
+
+modelM = AR(train['Money'])
+AR5fit1 = modelM.fit(maxlag=5,method='mle')
+print(f'Lag: {AR5fit1.k_ar}')
+print(f'Coefficients:\n{AR5fit1.params}')
+
+
+# In[35]:
+
+
+start=len(train)
+end=len(train)+len(test)-1
+z1 = pd.DataFrame(AR5fit1.predict(start=start, end=end, dynamic=False),columns=['Money'])
+
+
+# In[36]:
+
+
+z1
+
+
+# ### Invert the Transformation, Evaluate the Forecast
+
+# In[37]:
+
+
+# Add the most recent first difference from the training set to the forecast cumulative sum
+z1['Money1d'] = (df['Money'].iloc[-nobs-1]-df['Money'].iloc[-nobs-2]) + z1['Money'].cumsum()
+
+# Now build the forecast values from the first difference set
+z1['MoneyForecast'] = df['Money'].iloc[-nobs-1] + z1['Money1d'].cumsum()
+
+
+# In[38]:
+
+
+z1
+
+
+# In[39]:
+
+
+RMSE3 = rmse(df['Money'][-nobs:], z1['MoneyForecast'])
+
+print(f'Money VAR(5) RMSE: {RMSE1:.3f}')
+print(f'Money  AR(5) RMSE: {RMSE3:.3f}')
+
+
+# ## Personal Spending
+
+# In[40]:
+
+
+modelS = AR(train['Spending'])
+AR5fit2 = modelS.fit(maxlag=5,method='mle')
+print(f'Lag: {AR5fit2.k_ar}')
+print(f'Coefficients:\n{AR5fit2.params}')
+
+
+# In[41]:
+
+
+z2 = pd.DataFrame(AR5fit2.predict(start=start, end=end, dynamic=False),columns=['Spending'])
+z2
+
+
+# ### Invert the Transformation, Evaluate the Forecast
+
+# In[42]:
+
+
+# Add the most recent first difference from the training set to the forecast cumulative sum
+z2['Spending1d'] = (df['Spending'].iloc[-nobs-1]-df['Spending'].iloc[-nobs-2]) + z2['Spending'].cumsum()
+
+# Now build the forecast values from the first difference set
+z2['SpendingForecast'] = df['Spending'].iloc[-nobs-1] + z2['Spending1d'].cumsum()
+
+
+# In[43]:
+
+
+z2
+
+
+# In[44]:
+
+
+RMSE4 = rmse(df['Spending'][-nobs:], z2['SpendingForecast'])
+
+print(f'Spending VAR(5) RMSE: {RMSE2:.3f}')
+print(f'Spending  AR(5) RMSE: {RMSE4:.3f}')
+
+
+# <strong>CONCLUSION:</strong> It looks like the VAR(5) model did <em>not</em> do better than the individual AR(5) models. That's ok - we know more than we did before. In the next section we'll look at VARMA and see if the addition of a $q$ parameter helps. Great work!
--- timesSeriesAnalysis.py
+++ timesSeriesAnalysis.py
@@ -3419,76 +3419,215 @@ for x in df.query('holiday==1').index:
     ax.axvline(x=x, color='k', alpha = 0.3);
 
 
-# In[ ]:
+# # VAR(p)
+# ## Vector Autoregression
+# In our previous SARIMAX example, the forecast variable $y_t$ was influenced by the exogenous predictor variable, but not vice versa. That is, the occurrence of a holiday affected restaurant patronage but not the other way around.
+# 
+# However, there are some cases where variables affect each other. <a href='https://otexts.com/fpp2/VAR.html'>Forecasting: Principles and Practice</a> describes a case where changes in personal consumption expenditures $C_t$ were forecast based on changes in personal disposable income $I_t$.
+# > However, in this case a bi-directional relationship may be more suitable: an increase in $I_t$ will lead to an increase in $C_t$ and vice versa.<br>An example of such a situation occurred in Australia during the Global Financial Crisis of 2008–2009. The Australian government issued stimulus packages that included cash payments in December 2008, just in time for Christmas spending. As a result, retailers reported strong sales and the economy was stimulated. Consequently, incomes increased.
+# 
+# Aside from investigating multivariate time series, vector autoregression is used for
+# * <a href='https://www.statsmodels.org/devel/vector_ar.html#impulse-response-analysis'>Impulse Response Analysis</a> which involves the response of one variable to a sudden but temporary change in another variable
+# * <a href='https://www.statsmodels.org/devel/vector_ar.html#forecast-error-variance-decomposition-fevd'>Forecast Error Variance Decomposition (FEVD)</a> where the proportion of the forecast variance of one variable is attributed to the effect of other variables
+# * <a href='https://www.statsmodels.org/devel/vector_ar.html#dynamic-vector-autoregressions'>Dynamic Vector Autoregressions</a> used for estimating a moving-window regression for the purposes of making forecasts throughout the data sample
+# 
+# ### Formulation
+# We've seen that an autoregression AR(p) model is described by the following:
+# 
+# &nbsp;&nbsp;&nbsp;&nbsp; $y_{t} = c + \phi_{1}y_{t-1} + \phi_{2}y_{t-2} + \dots + \phi_{p}y_{t-p} + \varepsilon_{t}$
+# 
+# where $c$ is a constant, $\phi_{1}$ and $\phi_{2}$ are lag coefficients up to order $p$, and $\varepsilon_{t}$ is white noise.
+
+# A $K$-dimensional VAR model of order $p$, denoted <strong>VAR(p)</strong>, considers each variable $y_K$ in the system.<br>
+# 
+# For example, The system of equations for a 2-dimensional VAR(1) model is:
+# 
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{1,t} = c_1 + \phi_{11,1}y_{1,t-1} + \phi_{12,1}y_{2,t-1} + \varepsilon_{1,t}$<br>
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{2,t} = c_2 + \phi_{21,1}y_{1,t-1} + \phi_{22,1}y_{2,t-1} + \varepsilon_{2,t}$
+# 
+# where the coefficient $\phi_{ii,l}$ captures the influence of the $l$th lag of variable $y_i$ on itself,<br>
+# the coefficient $\phi_{ij,l}$ captures the influence of the $l$th lag of variable $y_j$ on $y_i$,<br>
+# and $\varepsilon_{1,t}$ and $\varepsilon_{2,t}$ are white noise processes that may be correlated.<br>
+# 
+# Carrying this further, the system of equations for a 2-dimensional VAR(3) model is:
+# 
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{1,t} = c_1 + \phi_{11,1}y_{1,t-1} + \phi_{12,1}y_{2,t-1} + \phi_{11,2}y_{1,t-2} + \phi_{12,2}y_{2,t-2} + \phi_{11,3}y_{1,t-3} + \phi_{12,3}y_{2,t-3} + \varepsilon_{1,t}$<br>
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{2,t} = c_2 + \phi_{21,1}y_{1,t-1} + \phi_{22,1}y_{2,t-1} + \phi_{21,2}y_{1,t-2} + \phi_{22,2}y_{2,t-2} + \phi_{21,3}y_{1,t-3} + \phi_{22,3}y_{2,t-3} + \varepsilon_{2,t}$<br><br>
+# 
+# and the system of equations for a 3-dimensional VAR(2) model is:
+# 
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{1,t} = c_1 + \phi_{11,1}y_{1,t-1} + \phi_{12,1}y_{2,t-1} + \phi_{13,1}y_{3,t-1} + \phi_{11,2}y_{1,t-2} + \phi_{12,2}y_{2,t-2} + \phi_{13,2}y_{3,t-2} + \varepsilon_{1,t}$<br>
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{2,t} = c_2 + \phi_{21,1}y_{1,t-1} + \phi_{22,1}y_{2,t-1} + \phi_{23,1}y_{3,t-1} + \phi_{21,2}y_{1,t-2} + \phi_{22,2}y_{2,t-2} + \phi_{23,2}y_{3,t-2} + \varepsilon_{2,t}$<br>
+# &nbsp;&nbsp;&nbsp;&nbsp;$y_{3,t} = c_3 + \phi_{31,1}y_{1,t-1} + \phi_{32,1}y_{2,t-1} + \phi_{33,1}y_{3,t-1} + \phi_{31,2}y_{1,t-2} + \phi_{32,2}y_{2,t-2} + \phi_{33,2}y_{3,t-2} + \varepsilon_{3,t}$<br><br>
+# 
+# The general steps involved in building a VAR model are:
+# * Examine the data
+# * Visualize the data
+# * Test for stationarity
+# * If necessary, transform the data to make it stationary
+# * Select the appropriate order <em>p</em>
+# * Instantiate the model and fit it to a training set
+# * If necessary, invert the earlier transformation
+# * Evaluate model predictions against a known test set
+# * Forecast the future
+# 
+# Recall that to fit a SARIMAX model we passed one field of data as our <em>endog</em> variable, and another for <em>exog</em>. With VAR, both fields will be passed in as <em>endog</em>.
+
+# <div class="alert alert-info"><h3>Related Functions:</h3>
+# <tt><strong>
+# <a href='https://www.statsmodels.org/stable/generated/statsmodels.tsa.vector_ar.var_model.VAR.html'>vector_ar.var_model.VAR</a></strong><font color=black>(endog[, exog, …])</font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fit VAR(p) process and do lag order selection<br>
+# <strong><a href='https://www.statsmodels.org/stable/generated/statsmodels.tsa.vector_ar.var_model.VARResults.html'>vector_ar.var_model.VARResults</a></strong><font color=black>(endog, …[, …])</font>&nbsp;&nbsp;Estimate VAR(p) process with fixed number of lags<br>
+# <strong><a href='https://www.statsmodels.org/stable/generated/statsmodels.tsa.vector_ar.dynamic.DynamicVAR.html'>vector_ar.dynamic.DynamicVAR</a></strong><font color=black>(data[, …])</font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Estimates time-varying vector autoregression (VAR(p)) using equation-by-equation least squares</tt>
+#    
+# <h3>For Further Reading:</h3>
+# <strong>
+# <a href='https://www.statsmodels.org/stable/vector_ar.html'>Statsmodels Tutorial:</a></strong>&nbsp;&nbsp;<font color=black>Vector Autoregressions</font><br>
+# <strong>
+# <a href='https://otexts.com/fpp2/VAR.html'>Forecasting: Principles and Practice:</a></strong>&nbsp;&nbsp;<font color=black>Vector Autoregressions</font><br>
+# <strong>
+# <a href='https://en.wikipedia.org/wiki/Vector_autoregression'>Wikipedia:</a></strong>&nbsp;&nbsp;<font color=black>Vector Autoregression</font>
+# </div>
+
+# ### Perform standard imports and load dataset
+# For this analysis we'll also compare money to spending. We'll look at the M2 Money Stock which is a measure of U.S. personal assets, and U.S. personal spending. Both datasets are in billions of dollars, monthly, seasonally adjusted. They span the 21 years from January 1995 to December 2015 (252 records).<br>
+# Sources: https://fred.stlouisfed.org/series/M2SL https://fred.stlouisfed.org/series/PCE
+
+# In[8]:
 
 
+import numpy as np
+import pandas as pd
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Load specific forecasting tools
+from statsmodels.tsa.api import VAR
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tools.eval_measures import rmse
+
+# Ignore harmless warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+# Load datasets
+df = pd.read_csv('./original/TSA_COURSE_NOTEBOOKS/Data/M2SL.csv',index_col=0, parse_dates=True)
+df.index.freq = 'MS'
+
+sp = pd.read_csv('./original/TSA_COURSE_NOTEBOOKS/Data/PCE.csv',index_col=0, parse_dates=True)
+sp.index.freq = 'MS'
 
 
-
-# In[ ]:
-
+# In[11]:
 
 
+df = df.join(sp)
+df = df.dropna()
+df.shape
 
 
-# In[ ]:
+# In[14]:
 
 
+df.columns = ["Money", "Spending"]
 
 
-
-# In[ ]:
-
+# In[16]:
 
 
+title = 'M2 Money Stock vs. Personal Consumption Expenditures'
+ylabel='Billions of dollars'
+xlabel=''
+
+ax = df['Spending'].plot(figsize=(12,5),title=title,legend=True)
+ax.autoscale(axis='x',tight=True)
+ax.set(xlabel=xlabel, ylabel=ylabel)
+df['Money'].plot(legend=True);
 
 
-# In[ ]:
+# ## Test for stationarity, perform any necessary transformations
+
+# In[17]:
 
 
+def adf_test(series,title=''):
+    """
+    Pass in a time series and an optional title, returns an ADF report
+    """
+    print(f'Augmented Dickey-Fuller Test: {title}')
+    result = adfuller(series.dropna(),autolag='AIC') # .dropna() handles differenced data
+    
+    labels = ['ADF test statistic','p-value','# lags used','# observations']
+    out = pd.Series(result[0:4],index=labels)
+
+    for key,val in result[4].items():
+        out[f'critical value ({key})']=val
+        
+    print(out.to_string())          # .to_string() removes the line "dtype: float64"
+    
+    if result[1] <= 0.05:
+        print("Strong evidence against the null hypothesis")
+        print("Reject the null hypothesis")
+        print("Data has no unit root and is stationary")
+    else:
+        print("Weak evidence against the null hypothesis")
+        print("Fail to reject the null hypothesis")
+        print("Data has a unit root and is non-stationary")
 
 
-
-# In[ ]:
-
+# In[18]:
 
 
+adf_test(df['Money'],title='Money')
 
 
-# In[ ]:
+# In[19]:
 
 
+adf_test(df['Spending'], title='Spending')
 
 
+# Neither variable is stationary, so we'll take a first order difference of the entire DataFrame and re-run the augmented Dickey-Fuller tests. It's advisable to save transformed values in a new DataFrame, as we'll need the original when we later invert the transormations and evaluate the model.
 
-# In[ ]:
-
-
-
+# In[20]:
 
 
-# In[ ]:
+df_transformed = df.diff()
 
 
+# In[21]:
 
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+df_transformed = df_transformed.dropna()
+adf_test(df_transformed['Money'], title='MoneyFirstDiff')
+print()
+adf_test(df_transformed['Spending'], title='SpendingFirstDiff')
 
 
+# Still they are not stationary. We will need to keep differencing until they are both stationary.
+# 
+# Note that we need to have the same number of rows for both series. 
+# 
+# So in case one of that was already statinary and the other not, we still need to difference both of them
+
+# In[22]:
 
 
+df_transformed = df_transformed.diff().dropna()
+adf_test(df_transformed['Money'], title='MoneySecondDiff')
+print()
+adf_test(df_transformed['Spending'], title='SpendingSecondDiff')
 
-# In[ ]:
+
+# ### Train/test split
+# It will be useful to define a number of observations variable for our test set. For this analysis, let's use 12 months.
+
+# In[23]:
 
 
+nobs=12  #number of observations
+train, test = df_transformed[0:-nobs], df_transformed[-nobs:]
 
+print(train.shape)
+print(test.shape)
 
 
 # In[ ]:
